5a7a9f14323328ad1ccd052e7fd6972c33138f9c
Bikram Mondal <devartistrybikram@yahoo.com>
2025-03-01 00:44:04 +0530

    Publish Files

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.owl-banner').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        center: true
+    });
+    $('.owl-gallery').owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 20,
+        stage: 20,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive:{
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
+});
